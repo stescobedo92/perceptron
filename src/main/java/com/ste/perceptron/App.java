f007acea112c2328985d82c222ff4a69de7517fe
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-09-04 19:31:11 -0700

    Update perceptron

--- src/main/java/com/ste/perceptron/App.java
+++ src/main/java/com/ste/perceptron/App.java
@@ -1,17 +1,28 @@
 package com.ste.perceptron;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
  * Hello world!
  */
 public final class App {
-    private App() {
-    }
+    static double[][] X = { {0,0},{1,0},{0,1},{1,1} };
+    static double[][] Y = { {0},{1},{1},{0} };
 
     /**
-     * Says hello to the world.
+     * Perceptron.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        NeuralNetwork nn = new NeuralNetwork(2, 10, 1);
+        List<Double> output;
+
+        nn.fit(X, Y, 50000);
+        double[][] input = { {0,0},{1,0},{0,1},{1,1} };
+
+        for(double[] d : input){
+            output = nn.predict(d);
+            System.out.println(output.toString());
+        }
     }
 }
