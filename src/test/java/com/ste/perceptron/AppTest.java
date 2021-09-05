a280377ce38a42e6e8f3f071249891d26056f766
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-09-04 17:36:53 -0700

    Initial commit

---
+++ src/test/java/com/ste/perceptron/AppTest.java
@@ -0,0 +1,18 @@
+package com.ste.perceptron;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
